47a5355f5064bbfb72f9d5dc2b06d8b708518059
Yuri_S <zebrawild@gmail.com>
2025-11-15 12:57:01 +0200

    added in server.ts cors for https://yuri3k.github.io/freshok-frontend/home

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,10 @@ const PORT = process.env.PORT || 5000
 // Настройка CORS
 app.use(cors({
   credentials: true,
-  origin: ['http://localhost:4200']
+  origin: [
+    'http://localhost:4200', 
+    'https://yuri3k.github.io',
+    'https://yuri3k.github.io/freshok-frontend/home']
 }))
 
 // Middleware для парсинга тела запроса (JSON и URL-encoded)
